7c6f13ae96780b5be3b1432d0996537934725f84
Stavinskaya_M <marina.st6277@gmail.com>
2023-07-11 22:27:00 +0600

    junit4one

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -9,24 +9,30 @@ public class CashbackHackServiceTest {
     @Test
     public void remain() {
         CashbackHackService service = new CashbackHackService();
+
         int actual = service.remain(200);
         int expected = 800;
+
         assertEquals(expected, actual);
     }
 
     @Test
     public void remainLargeAmount() {
         CashbackHackService service = new CashbackHackService();
+
         int actual = service.remain(1300);
         int expected = 700;
+
         assertEquals(expected, actual);
     }
 
     @Test
     public void remainEqualAmount() {
         CashbackHackService service = new CashbackHackService();
+
         int actual = service.remain(1000);
         int expected = 0;
+        
         assertEquals(expected, actual);
     }
 
